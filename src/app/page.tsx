b6eb2746285766fdda1434a1fbe0d54a0a3cc824
mskk3215 <m.kaaiweb@gmail.com>
2023-12-09 10:51:54 +0900

    fix:UI about Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import NotesList from "./components/notes-list";
 
 export default function Home() {
   return (
     <main>
-      <div className="m-10 text-center">Hello World🚀</div>
+      <div className="m-10 text-center">
+        <p>Hello World🚀</p>
+        <NotesList />
+      </div>
     </main>
   );
 }
