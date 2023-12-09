637a3c9f861d387d5aeb635b4fd0c52127ccb604
mskk3215 <m.kaaiweb@gmail.com>
2023-12-09 13:44:28 +0900

    feat:RefreshBtn add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import NotesList from "./components/notes-list";
 import TimerCounter from "./components/timer-counter";
 import Spinner from "./components/spinner";
+import RefreshBtn from "./components/refresh-btn";
 
 export default function Home() {
   return (
